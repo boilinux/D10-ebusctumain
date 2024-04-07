71125c3f82236e35cba71a0b36e2e3d8deaa193f
Stephen Wenceslao <stephenjohnwenceslao@gmail.com>
2024-04-07 21:11:18 +0800

    Update

--- web/themes/custom/ebus/js/custom.js
+++ web/themes/custom/ebus/js/custom.js
@@ -1,3 +1,9 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+(function ($, Drupal, window, document, undefined) {
+    $(document).ready(function(){
+        if ($('body.path-seat-available').length){
+            setTimeout(function () {
+                location.reload(true);
+            }, 3000);
+        }
+    });
+})(jQuery, Drupal, this, this.document);
