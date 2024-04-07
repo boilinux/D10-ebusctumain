3b6be9e39df5e1b02a20f8b76350fe822fea71a2
Stephen Wenceslao <stephenjohnwenceslao@gmail.com>
2024-04-07 18:52:42 +0800

    Update

--- web/modules/custom_module/src/Controller/CustomApi.php
+++ web/modules/custom_module/src/Controller/CustomApi.php
@@ -2,116 +2,143 @@
 
 namespace Drupal\custom_module\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Drupal\user\Entity\User;
-use Drupal\node\Entity\Node;
-use Drupal\file\Entity\File;
 use Drupal\Component\Serialization\Json;
+use Drupal\node\Entity\Node;
+// use Drupal\file\Entity\File;
+use Drupal\user\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
-class CustomApi
-{
-    public function post_data_log(Request $request)
-    {
-        $jsonDataResponse = new \stdClass();
-        $message = '';
-        $messageCode = 200;
+/**
+ * Custom api.
+ */
+class CustomApi {
+	public function post_data_log(Request $request) {
+		$jsonDataResponse = new \stdClass();
+		$message = '';
+		$messageCode = 200;
 
-        try {
-            $requestToken = $request->headers->get('Authorization');
-            $adminUser = User::load(1);
-            $restToken = $adminUser->field_rest_token->value;
+		try {
+			$requestToken = $request->headers->get('Authorization');
+			$adminUser = User::load(1);
+			$restToken = $adminUser->field_rest_token->value;
 
-            if ($requestToken == $restToken) {
+			if ($requestToken == $restToken) {
 
-                $passengerStatus = "";
-                $passengerType = "";
-                $seatAvailable = 0;
-                $latitude = "";
-                $longitude = "";
+				$passengerStatus = "";
+				$passengerType = "";
+				$seatAvailable = 0;
+				$latitude = "";
+				$longitude = "";
 
-                // Create node for data log
-                $bodyData = Json::decode($request->getContent());
+				// Create node for data log
+				$bodyData = Json::decode($request->getContent());
 
-                // from flutter app
-                if (!empty($bodyData['qrcode'])) {
-                    $strQrcode = $bodyData['qrcode'];
-                    if (in_array($strQrcode, ['student', 'senior_citizen', 'regular'])) {
-                        $passengerType = $bodyData['qrcode'];
-                        $passengerStatus = "get_off_the_bus";
-                        $latitude = $bodyData['latitude'];
-                        $longitude = $bodyData['longitude'];
+				// from flutter app
+				if (!empty($bodyData['qrcode'])) {
+					$strQrcode = $bodyData['qrcode'];
+					if (in_array($strQrcode, ['student', 'senior_citizen', 'regular'])) {
+						$passengerType = $bodyData['qrcode'];
+						$passengerStatus = "get_off_the_bus";
+						$latitude = $bodyData['latitude'];
+						$longitude = $bodyData['longitude'];
 
-                        // easpek
-                        system("sudo espeak-ng \"Hello Driver, Passenger want to get off the bus.\" -ven-us+f3 -s150 ");
-                    }
-                } else {
-                    $queryData = \Drupal::database()->query(
-                        "SELECT nfbsa.field_bus_seat_available_value AS seat_available, nfbc.field_bus_capacity_value AS capacity FROM node_field_data AS nfd
-                        LEFT JOIN node__field_bus_seat_available AS nfbsa ON nfbsa.entity_id = nfd.nid
+						// easpek
+						system("sudo espeak-ng \"Hello Driver, Passenger want to get off the bus.\" -ven-us+f3 -s150 ");
+						$queryData = \Drupal::database()->query(
+							"SELECT nfbsa.field_bus_seat_available_value AS seat_available, nfbc.field_bus_capacity_value AS capacity FROM node_field_data AS nfd
+                            LEFT JOIN node__field_bus_seat_available AS nfbsa ON nfbsa.entity_id = nfd.nid
                             LEFT JOIN node__field_bus_capacity AS nfbc ON nfbc.entity_id = nfd.nid
                             WHERE nfd.type = 'data_logs'
                             ORDER BY nfd.nid DESC
                             LIMIT 1"
-                    )->fetchAll();
+						)->fetchAll();
 
-                    if (!empty($queryData)) {
-                        $jsonResponse = new JsonResponse($queryData);
-                        \Drupal::logger('custom_module')->info($jsonResponse);
+						if (empty($queryData)) {
+							// no passenger records
+							$messageCode = 404;
+							$message = "No passenger record";
+							$jsonResponse = new JsonResponse(['message' => $message, 'status' => $messageCode, 'method' => 'POST', 'data' => $jsonDataResponse]);
 
-                        $oldPassengerSeat = $queryData[0]->seat_available;
-                        $busCapacity = $queryData[0]->capacity;
-                        if ($oldPassengerSeat > 0) {
-                            $seatAvailable = $oldPassengerSeat - 1;
-                        } else {
-                            // no available seat
-                            // disable printing of receipt
-                            $messageCode = 404;
-                            $message = "No available seat";
-                            $jsonResponse = new JsonResponse(['message' => $message, 'status' => $messageCode, 'method' => 'POST', 'data' => $jsonDataResponse]);
+							\Drupal::logger('custom_module')->info($jsonResponse);
 
-                            \Drupal::logger('custom_module')->info($jsonResponse);
+							return $jsonResponse;
+						}
+						$oldPassengerSeat = $queryData[0]->seat_available;
+						$busCapacity = $queryData[0]->capacity;
 
-                            return $jsonResponse;
-                        }
-                    } else {
-                        $seatAvailable = 23; // 23; because 24 seat capacity
-                    }
-                    $passengerStatus = $bodyData['passenger_status'];
-                    $passengerType = $bodyData['passenger_type'];
-                }
+						$seatAvailable = $oldPassengerSeat + 1; // increment new available seat
+						// easpek
+						system("sudo espeak-ng \"Seat Avaialbe now is " . $seatAvailable . ".\" -ven-us+f3 -s150 ");
 
-                // $date = \DateTime::createFromFormat('d-m-Y H:i:s', $bodyData['date'] . ' 00:00:00');
-                // $date = $date->getTimestamp();
-                $date = date("F j, Y, g:i a");
-                $title = 'Data log for ' . $passengerType . ' - ' . $passengerStatus . ' - ' . $date;
+					}
+				} else {
+					$queryData = \Drupal::database()->query(
+						"SELECT nfbsa.field_bus_seat_available_value AS seat_available, nfbc.field_bus_capacity_value AS capacity FROM node_field_data AS nfd
+                        LEFT JOIN node__field_bus_seat_available AS nfbsa ON nfbsa.entity_id = nfd.nid
+                        LEFT JOIN node__field_bus_capacity AS nfbc ON nfbc.entity_id = nfd.nid
+                        WHERE nfd.type = 'data_logs'
+                        ORDER BY nfd.nid DESC
+                        LIMIT 1"
+					)->fetchAll();
 
-                $node = Node::create([
-                    'type' => 'data_logs',
-                    'title' => $title,
-                    'field_type_of_passenger' => $passengerType,
-                    'field_passenger_status' => $passengerStatus,
-                    'field_bus_seat_available' => $seatAvailable,
-                    'field_location' => ['lat' => $latitude, 'lon' => $longitude, 'name' => 'location', 'zoom' => 15, 'type' => 'roadmap', 'width' => '500px', 'height' => '500px', 'marker' => 1, 'traffic' => 0, 'marker_icon' => '', 'controls' => 1],
-                    'uid' => 0,
-                ]);
+					if (!empty($queryData)) {
+						$jsonResponse = new JsonResponse($queryData);
+						\Drupal::logger('custom_module')->info($jsonResponse);
 
-                $node->save();
+						$oldPassengerSeat = $queryData[0]->seat_available;
+						$busCapacity = $queryData[0]->capacity;
+						if ($oldPassengerSeat > 0) {
+							$seatAvailable = $oldPassengerSeat - 1;
+						} else {
+							// no available seat
+							// disable printing of receipt
+							$messageCode = 404;
+							$message = "No available seat";
+							$jsonResponse = new JsonResponse(['message' => $message, 'status' => $messageCode, 'method' => 'POST', 'data' => $jsonDataResponse]);
 
-                $message = 'Successfully created data log for ' . $title;
-            } else {
-                // Invalid credentials
-                $message = 'Invalid Credentials';
-            }
-        } catch (\Exception $error) {
-            $message = $error;
-            $messageCode = 404;
-        }
+							\Drupal::logger('custom_module')->info($jsonResponse);
 
-        $jsonResponse = new JsonResponse(['message' => $message, 'status' => $messageCode, 'method' => 'POST', 'data' => $jsonDataResponse]);
+							return $jsonResponse;
+						}
+					} else {
+						$seatAvailable = 23; // 23; because 24 seat capacity
+					}
+					$passengerStatus = $bodyData['passenger_status'];
+					$passengerType = $bodyData['passenger_type'];
+				}
 
-        \Drupal::logger('custom_module')->info($jsonResponse);
+				// $date = \DateTime::createFromFormat('d-m-Y H:i:s', $bodyData['date'] . ' 00:00:00');
+				// $date = $date->getTimestamp();
+				$date = date("F j, Y, g:i a");
+				$title = 'Data log for ' . $passengerType . ' - ' . $passengerStatus . ' - ' . $date;
 
-        return $jsonResponse;
-    }
+				$node = Node::create([
+					'type' => 'data_logs',
+					'title' => $title,
+					'field_type_of_passenger' => $passengerType,
+					'field_passenger_status' => $passengerStatus,
+					'field_bus_seat_available' => $seatAvailable,
+					'field_location' => ['lat' => $latitude, 'lon' => $longitude, 'name' => 'location', 'zoom' => 15, 'type' => 'roadmap', 'width' => '500px', 'height' => '500px', 'marker' => 1, 'traffic' => 0, 'marker_icon' => '', 'controls' => 1],
+					'uid' => 0,
+				]);
+
+				$node->save();
+
+				$message = 'Successfully created data log for ' . $title;
+			} else {
+				// Invalid credentials
+				$message = 'Invalid Credentials';
+			}
+		} catch (\Exception $error) {
+			$message = $error;
+			$messageCode = 404;
+		}
+
+		$jsonResponse = new JsonResponse(['message' => $message, 'status' => $messageCode, 'method' => 'POST', 'data' => $jsonDataResponse]);
+
+		\Drupal::logger('custom_module')->info($jsonResponse);
+
+		return $jsonResponse;
+	}
 }
